f10ba3c51dc6d22a8700fe68137a9f12b8e07c49
Bruno Rawan <brunorawan.dev@gmail.com>
2023-11-22 23:31:35 -0300

    add linha 04

--- aula40.py
+++ aula40.py
@@ -1,7 +1,3 @@
-"""Calculadora 02"""
-
 while True:
-    sair = input('Quer sair? [s]im: ')
-    sair = sair.lower()
-    sair = sair.startswith('s')
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
     print(sair)
